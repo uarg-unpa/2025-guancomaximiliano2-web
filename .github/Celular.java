e947c4de745f3ff88160143c10468291e05666c2
guancomaximiliano2-web <guancomaximiliano2@gmail.com>
2025-11-04 14:55:46 +0000

    hice el mainn, cree dos objetos celular y tambien llame a los metodos

--- .github/Celular.java
+++ .github/Celular.java
@@ -55,14 +55,17 @@ public class Celular {
         }
     }
 
+    // Metodo para aplicar descuento
     public void aplicarDescuento(double descuento) {
         if (descuento > 0 && descuento <= 100) {
-            precio = precio - (precio * descuento / 100)
+            precio = precio - (precio * descuento / 100);
+            System.out.println("El nuevo precio es: $" + precio);
         } else {
             System.out.println("El descuento no puede ser menor a 0 ni mayor a 100");
         }
     }
 
+    // Metodo para mostrar informacion
     public void mostrarInformacion() {
         System.out.println("Marca: " + marca);
         System.out.println("Modelo: " + modelo);
@@ -70,11 +73,76 @@ public class Celular {
         System.out.println("Almacenamiento: " + almacenamiento);
     }
 
+    // Metodo para comparar precio con otro celular
     public Celular compararPrecio(Celular otroCel) {
         if (this.precio > otroCel.getPrecio()) {
             return this;
         } else {
             return otroCel;
         }
+    }
+}
+
+import java.util.Scanner;
+
+public class MainCelulares {
+    public static void main(String[] args) {
+
+        // Crear el Scanner
+        Scanner leer = new Scanner(System.in);
+
+        // Pedirle al usuario los datos del primer celular
+        System.out.println("Ingrese el la marca del celular 1: ");
+        String marca1 = leer.nextLine();
+
+        System.out.println("Ingrese el modelo del celular 1: ");
+        String modelo1 = leer.NextLine();
+
+        System.out.println("Ingrese el precio del celular 1: ");
+        double precio1 = leer.nextDouble();
+
+        System.out.println("Ingrese el almacenamiento del celular 1: ");
+        int almacenamiento1 = leer.nextInt();
+
+        Celular celuDeChaime = new Celular(marca1, modelo1, precio1, almacenamiento1);
+        celuDeChaime.mostrarInformacion();
+
+        // Limpiar el buffer
+        leer.nextLine();
+
+        // Pedirle al usuario los datos del celular 2
+        System.out.println("Ingrese la marca del celular 2: ");
+        String marca2 = leer.nextLine();
+
+        System.out.println("Ingrese el modelo del celular 2: ");
+        String modelo2 = leer.nextLine();
+
+        System.out.println("Ingrese el precio del celular 2: ");
+        double precio2 = leer.nextDouble();
+
+        System.out.println("Ingrese el almacenamiento del celular 2: ");
+        int almacenamiento2 = leer.nextInt();
+
+        // Limpiar el buffer
+        leer.nextLine();
+
+        Celular celuDeSube = new Celular(marca2, modelo2, precio2, almacenamiento2);
+        celuDeSube.mostrarInformacion();
+
+        // Invocar al metodo actualizarAlmacenamiento
+        System.out.println("Ingrese el nuevo almacenamiento para el celular 1: ");
+        int nuevoAlmacen = leer.nextInt();
+        celuDeChaime.actualizarAlmacenamiento(nuevoAlmacen);
+
+        // Invocar al metodo aplicarDescuento
+        System.out.println("Ingrese el descuento para el celular 2: ");
+        double descuento = leer.nextDouble();
+        celuDeSube.aplicarDescuento(descuento);
+
+        // Invocar al metodo compararPrecio
+        Celular esMasCaro = celuDeChaime.compararPrecio(celuDeSube);
+        esMasCaro.mostrarInformacion();
+
+        leer.close();
     }
 }
